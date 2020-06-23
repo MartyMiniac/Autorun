4570cb4f61cde0ed73750658055eb70f6d75a4d5
Nishant Choudhary <35103134+nishantc7@users.noreply.github.com>
2020-06-23 18:40:00 +0530

    final version base64 request

--- requests_test.py
+++ requests_test.py
@@ -1,12 +1,13 @@
 import requests
 
-url = "https://judge0.p.rapidapi.com/submissions"
+url = "https://judge0.p.rapidapi.com/submissions?base64_encoded=true"
+base64 = "I2luY2x1ZGUgPHN0ZGlvLmg+CmludCBtYWluKCkgewogICAvLyBwcmludGYoKSBkaXNwbGF5cyB0aGUgc3RyaW5nIGluc2lkZSBxdW90YXRpb24KICAgcHJpbnRmKCJIZWxsbywgV29ybGQhIik7CiAgIHJldHVybiAwOwp9"
 
-payload = "{ \"language_id\": 50, \"source_code\": \"I2luY2x1ZGUgPHN0ZGlvLmg+CgppbnQgbWFpbih2b2lkKSB7CiAgY2hhciBu\\nYW1lWzEwXTsKICBzY2FuZigiJXMiLCBuYW1lKTsKICBwcmludGYoImhlbGxv\\nLCAlc1xuIiwgbmFtZSk7CiAgcmV0dXJuIDA7Cn0=\\n\", \"stdin\": \"d29ybGQ=\\n\"}"
+payload = "{ \"language_id\": 50, \"source_code\": \"%s\\n\", \"stdin\": \"d29ybGQ=\\n\"}" % (base64)
 
 headers = { 
     'x-rapidapi-host': 'judge0.p.rapidapi.com',
-    'x-rapidapi-key': "339dba9922msh43e751b462b5016p1977d9jsna327bc002a18",
+    'x-rapidapi-key': "xxx",
     'content-type': "application/json",
     'accept': "application/json"
     }
